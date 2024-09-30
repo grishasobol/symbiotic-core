1f248c9e79adf1b5819b2dbf096ac150d63ce15d
Gregory Sobol <grishasobol@mail.ru>
2024-09-30 12:08:17 +0400

    0.8.25 -> ^0.8.25

--- src/contracts/common/StaticDelegateCallable.sol
+++ src/contracts/common/StaticDelegateCallable.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity 0.8.25;
+pragma solidity ^0.8.25;
 
 abstract contract StaticDelegateCallable {
     function staticDelegateCall(address target, bytes calldata data) external {
